df9312c6651ab8c7d25d61e82a0a3fb9bb42947d
Smythy Costa <smythy.costa@hotmail.com>
2018-01-09 06:56:54 -0300

    SaleDetail

--- core/models.py
+++ core/models.py
@@ -40,3 +40,18 @@ class Product(models.Model):
 class Sale(TimeStampedModel):
     customer = models.ForeignKey('Customer', related_name='customer_sale')
     seller = models.ForeignKey('Seller', related_name='seller_sale')
+    
+    def __str__(self):
+        return self.created
+
+
+class SaleDetail(models.Model):
+    sale = models.ForeignKey(Sale, related_name='sales_det')
+    product = models.ForeignKey(Product, related_name='product_det')
+    quantity = models.PositiveSmallIntegerField()
+    price_sale = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+
+    def get_subtotal(self):
+        return self.price_sale * (self.quantity or 0)
+
+    subtotal = property(get_subtotal)
